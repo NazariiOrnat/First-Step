2907ed13948adffd8329117c708b281ecef57c36
Nazar Ornat <nazarornat@mail.ru>
2016-03-05 19:11:37 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 $(document).ready(function (){
 
-    $("#jhome").click(function (){
+    $(."scroll").click(function (){
         $('html, body').animate({
             scrollTop: $("#home").offset().top
         }, 1500);
     });
-    $("#jabout-me").click(function (){
+    $(."scroll").click(function (){
         $('html, body').animate({
             scrollTop: $("#about-me").offset().top
         }, 1500);
     });
-    $("#jprof-skills").click(function (){
+    $(."scroll").click(function (){
         $('html, body').animate({
             scrollTop: $("#prof-sills").offset().top
         }, 1500);
     });
-    $("#jcontact").click(function (){
+    $(."scroll").click(function (){
         $('html, body').animate({
             scrollTop: $("#contact").offset().top
         }, 1500);
